90e14d2055142f8ea67f8b151d3199a5cafd9171
zx <zx@sf.u>
2013-09-05 21:18:37 +0300

    Админка 20%

---
+++ PadesEmpty/PadesEmpty/PadesEmpty/Default.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PadesEmpty
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void LinkButton1_Click(object sender, EventArgs e)
+        {
+            TextFromButton.InnerText = ((LinkButton) sender).Text;
+            switch (((LinkButton) sender).Text)
+            {
+                case "Товары":
+                    Button1.Visible = true;
+                    GridView2.Visible = true;
+                    break;
+                case "Категории":
+                    break;
+                case "Страницы":
+                    break;
+                case "Внешний вид":
+                    break;
+                case "SEO":
+                    break;
+                case "Информация":
+                    break;
+            }
+        }
+        protected void Products()
+        {
+            
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ PadesEmpty/PadesEmpty/PadesEmpty/Site.aspx
@@ -0,0 +1,56 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="PadesEmpty.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head id="Head1" runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+
+    <link  type="text/css" href="Site.css" rel="stylesheet"/>
+    
+</head>
+
+
+
+<body>
+<form id="form1" runat="server">
+
+
+
+   <div id="SiteHeader">
+   </div>
+   <div id="SitePage">
+       <div id="SiteMenu" >
+            
+
+           <div id="SiteNav">
+               <br />
+               <br />
+           </div>            
+           <div id="SiteSubscribe">
+               <br />
+               <br />
+           </div> 
+
+           <br />
+
+        </div>
+        <div id="SiteContent" >
+            
+
+            <div id="SiteSection">
+                <p>Раздел</p>
+            </div>
+            
+
+        </div>
+       <span id="Login" >&nbsp; Вход
+       </span>
+    </div>
+
+
+
+</form>
+</body>
+</html>
